e0d5d3d16e710344774d44589320fbf9632de6a7
ArsenyTask <166523357+ArsenyTask@users.noreply.github.com>
2024-04-22 16:41:58 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+from math import pi
+radius = 5
+a = 5
+print('площадь круга:-', round(pi *radius**2, 2))
+print('длинна окружности:', round(pi * radius * 2, 2))
+print('периметер квадрата:', a * 4)
+print('площадь квадрата:' , a**2)
